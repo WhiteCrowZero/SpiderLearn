e30f6a21c12dd10dda7f03fd05b93f7ff34cce66
WhiteCrowZero <150656436+WhiteCrowZero@users.noreply.github.com>
2024-09-14 19:47:11 +0800

    添加注释

--- 01_百度首页爬取.py
+++ 01_百度首页爬取.py
@@ -1,10 +1,13 @@
+# 使用Python自带的库urllib进行最简单的爬取测试
 from urllib.request import urlopen
 
 url = f'http://www.baidu.com'
-res = urlopen(url)
+res = urlopen(url)        # 对要爬取的网址进行请求
 
+# 打印一下，查看爬取到的结果
 # print(res.read().decode('utf-8'))
 
+# 打开文件并把爬取到的结果存储到本地
 file_path = './result/01_baidu.html'
 with open(file_path, 'w', encoding='utf-8') as f:
     f.write(res.read().decode('utf-8'))
